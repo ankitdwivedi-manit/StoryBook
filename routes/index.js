de85f3ac8be1d3e3487461aa35e5c6993a78eb60
ankit <kp19nimcet23@gmail.com>
2021-06-11 18:23:37 +0530

    Init Commit

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+const Story = require('../models/Story')
+
+const {ensureAuth, ensureGuest} = require('../middleware/auth')
+
+//login landing page
+//GET /
+router.get('/', ensureGuest , (req, res) => {
+    res.render('login', {
+        layout : 'login'
+    })
+})
+
+//dashboard
+//GET /dashboard
+router.get('/dashboard',ensureAuth, async (req, res) => {
+
+    try {
+        const stories = await Story.find({user : req.user.id}).lean()
+        res.render('dashboard', {
+            name : req.user.firstName,
+            stories
+        })
+    } catch (error) {
+        console.error(error)
+        res.render('error/500')
+    }
+})
+
+module.exports = router
